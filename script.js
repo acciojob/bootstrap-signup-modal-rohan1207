c345a5379bf08f7cbf5270891a92604541e672bd
Rohan Ambhore <142285944+rohan1207@users.noreply.github.com>
2025-01-30 11:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your JS code here. If required.
+const collapseElementList = document.querySelectorAll('.collapse')
+const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
